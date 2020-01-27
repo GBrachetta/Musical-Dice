5c5c3fc0fd763369b58cba1417fa875a2b08d1aa
Guillermo Brachetta <brachetta@me.com>
2020-01-27 09:38:02 +0100

    Form clearing after submission
    Unchecked checkbox
    Assigned value to checkbox

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -16,29 +16,31 @@
 //   return true;
 // }
 
-
-
 var myform = $("form#myform");
-myform.submit(function(event){
-	event.preventDefault();
+myform.submit(function(event) {
+  event.preventDefault();
 
-	var params = myform.serializeArray().reduce(function(obj, item) {
-     obj[item.name] = item.value;
-     return obj;
+  var params = myform.serializeArray().reduce(function(obj, item) {
+    obj[item.name] = item.value;
+    return obj;
   }, {});
 
   var service_id = "gmail";
 
   var template_id = "dice_game";
   myform.find("button").text("Sending...");
-  emailjs.send(service_id, template_id, params)
-  	.then(function(){ 
-       alert("Sent!");
-       myform.find("button").text("Submit");
-     }, function(err) {
-       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-       myform.find("button").text("Submit");
-    });
+  emailjs.send(service_id, template_id, params).then(
+    function() {
+      myform.find("input").val("");
+      myform.find("textarea").val("");
+      alert("Sent!");
+      myform.find("button").text("Submit");
+    },
+    function(err) {
+      alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+      myform.find("button").text("Submit");
+    }
+  );
 
   return false;
 });
