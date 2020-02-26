ecc480a12c6fc93ea24198ddb549a10663a87b99
Guillermo Brachetta <brachetta@me.com>
2020-02-26 13:32:09 +0100

    Re-indented js files for better legibility

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,82 +2,82 @@
  * Constants
  */
 const zeroPadd = n => (n < 10 ? "0" + n : n), // Function to padd a number with '0'
-  alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"], // For randomising array and creating grid
-  $musicGrid = $("#music-grid"),
-  $playButton = $("#play-minuetto"),
-  $pauseButton = $("#pause-button"),
-  $randomiseButton = $("#btn-randomise"),
-  $checkboxes = $("#checkboxes-minuetti"),
-  mp3list = alphabet.map(item => ({
-    name: `Minuetto ${item}`,
-    path: `assets/music/minuetto${item.toUpperCase()}.mp3`
-  })); // Defines the array of objects containing all full minuetti
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"], // For randomising array and creating grid
+    $musicGrid = $("#music-grid"),
+    $playButton = $("#play-minuetto"),
+    $pauseButton = $("#pause-button"),
+    $randomiseButton = $("#btn-randomise"),
+    $checkboxes = $("#checkboxes-minuetti"),
+    mp3list = alphabet.map(item => ({
+        name: `Minuetto ${item}`,
+        path: `assets/music/minuetto${item.toUpperCase()}.mp3`
+    })); // Defines the array of objects containing all full minuetti
 
 /**
  * Variables
  */
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"], // For randomising array and creating grid
-  randomIDs = [],
-  sequence = [],
-  $allMP3 = [],
-  isPlaying = false,
-  single = null; // Will handle a single Howler sound on cell click
+    randomIDs = [],
+    sequence = [],
+    $allMP3 = [],
+    isPlaying = false,
+    single = null; // Will handle a single Howler sound on cell click
 
 /**
  * Generates random selections from selected letters
  */
 function randomise() {
-  $playButton.prop("disabled", false).text("Play Minuetto"); // Restores play button after new randomisation
-  $musicGrid.find(".selected").removeClass("selected");
-  $(".bar")
-    .removeClass("playing disabled")
-    .prop("disabled", false); // Restores clickalability of grid
+    $playButton.prop("disabled", false).text("Play Minuetto"); // Restores play button after new randomisation
+    $musicGrid.find(".selected").removeClass("selected");
+    $(".bar")
+        .removeClass("playing disabled")
+        .prop("disabled", false); // Restores clickalability of grid
 
-  for (let i = 1; i <= 12; i++) {
-    const randomIndex = Math.floor(Math.random() * letters.length);
-    randomIDs[i - 1] = letters[randomIndex] + zeroPadd(i);
-  }
-  console.log(randomIDs);
+    for (let i = 1; i <= 12; i++) {
+        const randomIndex = Math.floor(Math.random() * letters.length);
+        randomIDs[i - 1] = letters[randomIndex] + zeroPadd(i);
+    }
+    console.log(randomIDs);
 
-  // Makes grid cells selected
-  randomIDs.forEach(id => $(`#${id}`).addClass("selected"));
+    // Makes grid cells selected
+    randomIDs.forEach(id => $(`#${id}`).addClass("selected"));
 
-  createSequence();
+    createSequence();
 }
 
 /**
  * Generates grid from user-selected values
  */
 function buildGrid() {
-  // Rebuilds the array of letters only from checked checkboxes
-  letters = $checkboxes
-    .find(":checkbox:checked")
-    .get()
-    .map(el => el.value);
+    // Rebuilds the array of letters only from checked checkboxes
+    letters = $checkboxes
+        .find(":checkbox:checked")
+        .get()
+        .map(el => el.value);
 
-  const noSelection = letters.length === 0;
+    const noSelection = letters.length === 0;
 
-  // Hides elements if no letters selected
-  $playButton.prop("disabled", noSelection);
-  $randomiseButton.prop("disabled", letters.length < 2);
-  $musicGrid.toggle(!noSelection);
+    // Hides elements if no letters selected
+    $playButton.prop("disabled", noSelection);
+    $randomiseButton.prop("disabled", letters.length < 2);
+    $musicGrid.toggle(!noSelection);
 
-  // Does nothing if no ckeckbox was selected (no letters)
-  if (noSelection) return; // EXIT FUNCTION HERE!
+    // Does nothing if no ckeckbox was selected (no letters)
+    if (noSelection) return; // EXIT FUNCTION HERE!
 
-  // else...
-  let html = "";
-  for (let i = 0; i < letters.length; i++) {
-    html += `<div id="music-row-${letters[i]}" class="row no-gutters">`;
-    for (let j = 1; j <= 12; j++) {
-      const columnID = letters[i] + zeroPadd(j);
-      html += `<div class="col-1"><button id="${columnID}" class="btn bar song">${columnID.toUpperCase()}</button></div>`;
+    // else...
+    let html = "";
+    for (let i = 0; i < letters.length; i++) {
+        html += `<div id="music-row-${letters[i]}" class="row no-gutters">`;
+        for (let j = 1; j <= 12; j++) {
+            const columnID = letters[i] + zeroPadd(j);
+            html += `<div class="col-1"><button id="${columnID}" class="btn bar song">${columnID.toUpperCase()}</button></div>`;
+        }
+        html += "</div>";
     }
-    html += "</div>";
-  }
 
-  $musicGrid.html(html);
-  randomise(); // Calls randomise to allow loading MP3 files
+    $musicGrid.html(html);
+    randomise(); // Calls randomise to allow loading MP3 files
 }
 
 /**
@@ -85,94 +85,94 @@ function buildGrid() {
  * Populates sequence variable with Howler Objects
  */
 function createSequence() {
-  isPlaying = false;
+    isPlaying = false;
 
-  if (sequence.length) {
-    sequence.forEach(sound => sound.unload());
-  }
+    if (sequence.length) {
+        sequence.forEach(sound => sound.unload());
+    }
 
-  // Rebuilds sequence array with Howler objects
-  sequence = randomIDs.map((id, i) => {
-    return new Howl({
-      src: `assets/music/${id}.mp3`,
-      onplay: function() {
-        $(`#${id}`).addClass("playing"); // Adds class to bar currently playing
-      },
-      onend: function() {
-        $(`#${id}`).removeClass("playing"); // Removes class of bar just played
-        // Handle all bars but the last one
-        if (i < randomIDs.length - 1) {
-          sequence[i + 1].play(); // Play next bar
-        } else {
-          isPlaying = false;
-          $playButton.text("Play Again"); // Restores play button after song
-          $(".bar")
-            .prop("disabled", false)
-            .removeClass("playing disabled"); // Restores grid after song
-        }
-      }
+    // Rebuilds sequence array with Howler objects
+    sequence = randomIDs.map((id, i) => {
+        return new Howl({
+            src: `assets/music/${id}.mp3`,
+            onplay: function() {
+                $(`#${id}`).addClass("playing"); // Adds class to bar currently playing
+            },
+            onend: function() {
+                $(`#${id}`).removeClass("playing"); // Removes class of bar just played
+                // Handle all bars but the last one
+                if (i < randomIDs.length - 1) {
+                    sequence[i + 1].play(); // Play next bar
+                } else {
+                    isPlaying = false;
+                    $playButton.text("Play Again"); // Restores play button after song
+                    $(".bar")
+                        .prop("disabled", false)
+                        .removeClass("playing disabled"); // Restores grid after song
+                }
+            }
+        });
     });
-  });
 }
 
 /**
  * Plays sequence
  */
 function playSequence() {
-  stopAll(); // Stop all sounds before playing a sequence
-  isPlaying = true;
-  sequence[0].play();
-  $playButton.text("Stop minuetto");
-  // Disables grid while playing
-  $(".bar") // can remove these lines now if I want the user to fully interact with all buttons
-    .removeClass("playing")
-    .addClass("disabled")
-    .prop("disabled", true);
+    stopAll(); // Stop all sounds before playing a sequence
+    isPlaying = true;
+    sequence[0].play();
+    $playButton.text("Stop minuetto");
+    // Disables grid while playing
+    $(".bar") // can remove these lines now if I want the user to fully interact with all buttons
+        .removeClass("playing")
+        .addClass("disabled")
+        .prop("disabled", true);
 }
 
 /**
  * Stops all (to be called before playing any sound)
  */
 function stopAll() {
-  stopSequence();
-  stopSingle();
-  $allMP3.forEach($el => $el.trigger("stop"));
+    stopSequence();
+    stopSingle();
+    $allMP3.forEach($el => $el.trigger("stop"));
 }
 
 /**
  * Stops sequence
  */
 function stopSequence() {
-  isPlaying = false;
-  sequence.forEach(bar => bar.stop());
-  $playButton.text("Play minuetto");
-  // Restores grid when clicked stop
-  $(".bar") // Can remove if decided to re-enable grid while playing
-    .removeClass("playing disabled")
-    .prop("disabled", false);
+    isPlaying = false;
+    sequence.forEach(bar => bar.stop());
+    $playButton.text("Play minuetto");
+    // Restores grid when clicked stop
+    $(".bar") // Can remove if decided to re-enable grid while playing
+        .removeClass("playing disabled")
+        .prop("disabled", false);
 }
 
 /**
  * Stops single
  */
 function stopSingle() {
-  // Handle already playing single bar sound
-  if (single && single.playing()) {
-    single.stop();
-  }
+    // Handle already playing single bar sound
+    if (single && single.playing()) {
+        single.stop();
+    }
 }
 
 /**
  * Toggles sequence play / stop state
  */
 function togglePlaySequence() {
-  isPlaying = !isPlaying; // Toggle isPlaying flag
+    isPlaying = !isPlaying; // Toggle isPlaying flag
 
-  if (isPlaying) {
-    playSequence();
-  } else {
-    stopSequence();
-  }
+    if (isPlaying) {
+        playSequence();
+    } else {
+        stopSequence();
+    }
 }
 
 /**
@@ -187,66 +187,66 @@ $playButton.on("click", togglePlaySequence); // Play / Stop sequence
 // jQuery .on() method with dynamic handler for .bar elements
 // Delegates listeners to parent element
 $musicGrid.on("click", ".bar", function() {
-  const $this = $(this);
+    const $this = $(this);
 
-  // Handle already playing single bar sound
-  stopAll();
+    // Handle already playing single bar sound
+    stopAll();
 
-  // Set new sound and play it
-  single = new Howl({
-    src: [`assets/bars/${this.id}.mp3`],
-    onplay: function() {
-      $this.addClass("playing"); // Adds class to show which bit is playing
-    },
-    onstop: function() {
-      $this.removeClass("playing"); // Removes class of bit just played
-    },
-    onend: function() {
-      $this.removeClass("playing"); // Removes class of bit just played
-    }
-  });
-  single.play();
+    // Set new sound and play it
+    single = new Howl({
+        src: [`assets/bars/${this.id}.mp3`],
+        onplay: function() {
+            $this.addClass("playing"); // Adds class to show which bit is playing
+        },
+        onstop: function() {
+            $this.removeClass("playing"); // Removes class of bit just played
+        },
+        onend: function() {
+            $this.removeClass("playing"); // Removes class of bit just played
+        }
+    });
+    single.play();
 });
 
 // Smooth scrolling, from https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
 $("a").on("click", function(event) {
-  let hash = this.hash;
-  if (!hash) return; // Do nothing if no hash. Else... animate
-  event.preventDefault();
-  $("html, body").animate({ scrollTop: $(hash).offset().top }, 800, function() {
-    window.location.hash = hash;
-  });
+    let hash = this.hash;
+    if (!hash) return; // Do nothing if no hash. Else... animate
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: $(hash).offset().top }, 800, function() {
+        window.location.hash = hash;
+    });
 });
 
 // Buttons to preview full minuetti
 const $newMP3 = mp3 => {
-  const sound = new Howl({ src: mp3.path }); // Howl
-  // $ ELEMENT
-  const $element = $(`<div/>`, {
-    class: "btn col-6 col-md-3 individual-minuetto btn-primary btn-lg ",
-    text: mp3.name,
-    on: {
-      click() {
-        $(this).trigger(sound.playing() ? "stop" : "play");
-      },
-      play() {
-        stopAll();
-        $(this)
-          .text(`Stop ${mp3.name}`)
-          .addClass("minuetto-playing");
-        sound.play();
-      },
-      stop() {
-        $(this)
-          .text(mp3.name)
-          .removeClass("minuetto-playing");
-        sound.stop();
-      }
-    }
-  });
-  // When the sound ends, triggers the $element stop() event
-  sound.on("end", () => $element.trigger("stop"));
-  return $element;
+    const sound = new Howl({ src: mp3.path }); // Howl
+    // $ ELEMENT
+    const $element = $(`<div/>`, {
+        class: "btn col-6 col-md-3 individual-minuetto btn-primary btn-lg ",
+        text: mp3.name,
+        on: {
+            click() {
+                $(this).trigger(sound.playing() ? "stop" : "play");
+            },
+            play() {
+                stopAll();
+                $(this)
+                    .text(`Stop ${mp3.name}`)
+                    .addClass("minuetto-playing");
+                sound.play();
+            },
+            stop() {
+                $(this)
+                    .text(mp3.name)
+                    .removeClass("minuetto-playing");
+                sound.stop();
+            }
+        }
+    });
+    // When the sound ends, triggers the $element stop() event
+    sound.on("end", () => $element.trigger("stop"));
+    return $element;
 };
 
 /**
@@ -256,8 +256,8 @@ $allMP3 = mp3list.map($newMP3); // Populate array of $ elements
 $("#minuetti").append($allMP3); // Append to div
 
 const checkboxesHTML = alphabet.reduce((_html, letter, i) => {
-  const checkboxID = `checkbox-${i + 1}`;
-  return (_html += `<div class = "col-3 col-md-2 col-lg-1">
+    const checkboxID = `checkbox-${i + 1}`;
+    return (_html += `<div class = "col-3 col-md-2 col-lg-1">
         <input type="checkbox" id="${checkboxID}" value="${letter.toLowerCase()}" checked />
         <label for="${checkboxID}">${letter}</label>
     </div>`);
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,38 +1,38 @@
-(function () {
-  emailjs.init("user_bXMwDjNNrfAYxbs0D1MYH");
+(function() {
+    emailjs.init("user_bXMwDjNNrfAYxbs0D1MYH");
 })();
 let myform = $("form#form-contact");
 myform.submit(function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  let params = myform.serializeArray().reduce(function(obj, item) {
-    obj[item.name] = item.value;
-    return obj;
-  }, {});
+    let params = myform.serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
 
-  let service_id = "gmail";
+    let service_id = "gmail";
 
-  let template_id = "dice_game";
-  myform.find("button").text("Sending...");
-  emailjs.send(service_id, template_id, params).then(
-    function() {
-      myform.find("input").val("");
-      myform.find("textarea").val("");
-      myform.find("button").text("Submit");
-    },
-    function(err) {
-      alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-      myform.find("button").text("Submit");
-    }
-  );
-  let messages = document.getElementById("messages");
-  messages.classList.add("message-style");
-  let message = document.createTextNode("Your email was successfully sent");
+    let template_id = "dice_game";
+    myform.find("button").text("Sending...");
+    emailjs.send(service_id, template_id, params).then(
+        function() {
+            myform.find("input").val("");
+            myform.find("textarea").val("");
+            myform.find("button").text("Submit");
+        },
+        function(err) {
+            alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+            myform.find("button").text("Submit");
+        }
+    );
+    let messages = document.getElementById("messages");
+    messages.classList.add("message-style");
+    let message = document.createTextNode("Your email was successfully sent");
 
-  messages.appendChild(message);
-  setTimeout(function() {
-    messages.classList.remove("message-style");
-    messages.removeChild(message);
-  }, 5000);
-  return false;
+    messages.appendChild(message);
+    setTimeout(function() {
+        messages.classList.remove("message-style");
+        messages.removeChild(message);
+    }, 5000);
+    return false;
 });
