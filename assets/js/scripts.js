eeec57ef70f51d0026bc8a1dc2c73c14a8c68a59
Guillermo Brachetta <brachetta@me.com>
2020-02-28 16:23:10 +0100

    Added back to top arrow (not having correct transition yet)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -214,6 +214,33 @@ $("a").on("click", function(event) {
 });
 
 /**
+ * Begin provisory method back to top arrow 
+ * from http://html-tuts.com/back-to-top-button-jquery/
+ * (transition for the arrow to appear doesn't work when applied color transition CSS)
+ */
+$("body").prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > amountScrolled) {
+        $("a.back-to-top").fadeIn("slow");
+    } else {
+        $("a.back-to-top").fadeOut("slow");
+    }
+});
+
+$("a.back-to-top, a.simple-back-to-top").click(function() {
+    $("html, body").animate(
+        {
+            scrollTop: 0
+        },
+        800
+    );
+    return false;
+});
+
+/**
  * Creates uttons to play original minuetti
  */
 function newMP3(mp3) {
